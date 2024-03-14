ecf19a615c9d9c5d6f54115fcb435806914c9070
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-15 00:54:42 +0200

    Time: 1 ms (60.90%), Space: 44.8 MB (62.75%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,12 +1,40 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        for (int i  = 0; i < nums.length; i++){
-            for (int j = i + 1; j < nums.length; j++){
-                if (nums[j] % 2 == 0 && nums[i] % 2 != 0){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+        int counteven = 0; //3
+        int countodd = 0; //3
+        int o = 0;
+        int g = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] % 2 == 0){
+                counteven++;
+            }
+            else {
+                countodd++;
+            }
+        }
+        int[] evenArr = new int[counteven];
+        int[] oddArr = new int[countodd];
+
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] % 2 == 0){
+                evenArr[g] = nums[i];
+                g++;
+            }
+            else {
+                oddArr[o] = nums[i];
+                o++;
+            }
+        }
+        int e1 = 0;
+        int o1 = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (i < evenArr.length){
+                nums[i] = evenArr[e1];
+                e1++;
+            }
+            else {
+                nums[i] = oddArr[o1];
+                o1++;
             }
         }
         return nums;
